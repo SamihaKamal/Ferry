d370293321ef24f8bc0cdcf441d70032a8d21035
samiha <samihakamal.7664@gmail.com>
2023-12-07 21:27:18 +0000

    Tabs completed

--- Ferry/components/Tab.js
+++ Ferry/components/Tab.js
@@ -9,9 +9,10 @@ import { View, TouchableOpacity } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
+
 const CustomAddPostButton = ({children, onPress}) => (
   <TouchableOpacity style={{
-    top: -30,
+    top: -20,
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: '#7f5Df0',
@@ -19,9 +20,7 @@ const CustomAddPostButton = ({children, onPress}) => (
       width: 0,
       height: 10,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.5,
-    elevation: 5
+    
   }}
   onPress={onPress} >
     <View style={{
@@ -37,14 +36,15 @@ const CustomAddPostButton = ({children, onPress}) => (
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{
+      tabBarShowLabel: false,
+      
+    }}>
       <Tab.Screen name="Home" component={HomeScreen}
       options={{
         title: 'Home',
         tabBarIcon: ({ focused, color, size }) => {
-          // This is just to test out different colours for the icon will change later
-          // When I have a colour scheme cough
-          const iconColour = focused ? 'red' : 'green';
+          const iconColour = focused ? 'red' : 'grey';
           return <Ionicons name={focused ? 'ios-home' : 'ios-home-outline'} size={size} color={iconColour}/>
 
         },
@@ -53,13 +53,15 @@ function MyTabs() {
       options={{
         title: 'Following',
         tabBarIcon: ({ focused, color, size }) => {
-          return <Ionicons name={focused ? 'bookmark' : 'bookmark-outline'} color={color} size={size} />
+          const iconColour = focused ? 'red' : 'grey';
+          return <Ionicons name={focused ? 'bookmark' : 'bookmark-outline'} color={iconColour} size={size} />
         }
       }}/>
       <Tab.Screen name="Add Post" component={PostScreen} 
-      options={{
+      options={{ 
         tabBarIcon: ({ focused, color, size }) => {
-          return <Ionicons name={'add'} color={color} size={size} />
+          const iconColour = focused ? 'grey' : 'white';
+          return <Ionicons name={'add'} color={iconColour} size={size} />
         },
         tabBarButton: (props) => (
           <CustomAddPostButton {...props} />
@@ -69,14 +71,16 @@ function MyTabs() {
       options={{
         title: 'Pages',
         tabBarIcon: ({ focused, color, size }) => {
-          return <Ionicons name={focused ? 'earth' : 'earth-outline'} color={color} size={size} />
+          const iconColour = focused ? 'red' : 'grey';
+          return <Ionicons name={focused ? 'earth' : 'earth-outline'} color={iconColour} size={size} />
         }
       }}/>
       <Tab.Screen name="Chat" component={ChatScreen} 
       options={{
         title: 'Chat',
         tabBarIcon: ({ focused, color, size }) => {
-          return <Ionicons name={focused ? 'chatbubble' : 'chatbubble-outline'} color={color} size={size} />
+          const iconColour = focused ? 'red' : 'grey';
+          return <Ionicons name={focused ? 'chatbubble' : 'chatbubble-outline'} color={iconColour} size={size} />
         }
       }}/>
     </Tab.Navigator>
